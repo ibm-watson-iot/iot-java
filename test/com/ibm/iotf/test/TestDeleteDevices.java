8ea420f8ed839e05ea490b766584c2e23e1c23d6
Amit M Mangalvedkar <amitmangalvedkar@gmail.com>
2015-02-19 20:32:08 +0530

    Removed superfluous comments

--- test/com/ibm/iotf/test/TestDeleteDevices.java
+++ test/com/ibm/iotf/test/TestDeleteDevices.java
@@ -1,6 +1,5 @@
 package com.ibm.iotf.test;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -18,17 +17,9 @@ import junit.framework.TestCase;
 public class TestDeleteDevices extends TestCase {
 	@Test
 	public void testDeleteDevice(){
-		/*String deviceType = "iotsample-arduino";
-		String deviceId = "00aaaaaaaa02";
-		Properties opt = new Properties();
-		//opt.setProperty("authKey", "a-uguhsp-t54ipe1bk7");
-		//opt.setProperty("authToken", "y1Viw)kxmbXeLXMo!m");
-		opt.put("authKey", "a-uguhsp-8ya0dmsjh9");
-		opt.put("authToken", ")2lMPstbsqkOTs@s&M");*/
+
 		try{
 			Properties opt = new Properties();
-			//opt.setProperty("authKey", "a-uguhsp-t54ipe1bk7");
-			//opt.setProperty("authToken", "y1Viw)kxmbXeLXMo!m");
 			String deviceType = "";
 			String deviceId = "";
 			String authKey = "";
@@ -47,52 +38,31 @@ public class TestDeleteDevices extends TestCase {
 				String key = (String) enuKeys.nextElement();
 				System.out.println(key + ": " );
 				if(key.equals("deviceType") ){
-					
 					deviceType = properties.getProperty(key);
-					
 				}
 				if(key.equals("deviceId")){
 					deviceId = properties.getProperty(key);
-					
 				}
 				if(key.equals("authKey")){
 					authKey = properties.getProperty(key);
-					
 				}
 				if(key.equals("authToken")){
 					authToken = properties.getProperty(key);
-					
 				}
-				//System.out.println(key + ": " + value);
 			}
-		
-			
-			
-			
-			//opt.put("authKey", "a-uguhsp-8ya0dmsjh9");
-			//opt.put("authToken", ")2lMPstbsqkOTs@s&M");
-			
+
 			opt.put("authKey", authKey);
 			opt.put("authToken", authToken);
 
 		DeviceFactory factory = new DeviceFactory(opt);
-		//String metadata = "";
-		//boolean device = factory.deleteDevice(deviceType, deviceId);
-		
-			assertEquals("Device didn't exist...", false, factory.deleteDevice(deviceType, deviceId));
-			assertEquals("Device deregistered....", true, factory.deleteDevice(deviceType, deviceId));
-			
-		
+		assertEquals("Device didn't exist...", false, factory.deleteDevice(deviceType, deviceId));
+		assertEquals("Device deregistered....", true, factory.deleteDevice(deviceType, deviceId));
 		//assertEquals("A test for Hello World String", "Hello World", hello.sayHello());
-			
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		}
-
-
-
+	}
 }
