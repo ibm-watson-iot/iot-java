cf29dc3a6112c3b6d751b4b710c83f0038da5354
Amit M Mangalvedkar <amitmangalvedkar@gmail.com>
2015-03-06 11:56:06 +0530

    Added test cases on Surbhi's behalf

--- test/com/ibm/iotf/test/TestDeleteDevices.java
+++ test/com/ibm/iotf/test/TestDeleteDevices.java
@@ -1,14 +1,16 @@
-package com.ibm.iotf.test;
+package com.ibm.iotf.client.device.test;
+
 
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Enumeration;
+
 import java.util.Properties;
 
 import org.junit.Test;
+
 
 import com.ibm.iotf.client.api.DeviceFactory;
 
@@ -17,52 +19,31 @@ import junit.framework.TestCase;
 public class TestDeleteDevices extends TestCase {
 	@Test
 	public void testDeleteDevice(){
+		
 		try{
 			Properties opt = new Properties();
-			String deviceType = "";
-			String deviceId = "";
-			String authKey = "";
-			String authToken = "";
+		
 			
-			File file = new File("src/test.properties");
+			File file = new File("src/com/ibm/iotf/client/device/test/test.properties");
 			FileInputStream fileInput = new FileInputStream(file);
 			Properties properties = new Properties();
 			properties.load(fileInput);
 			fileInput.close();
 
-			Enumeration<Object> enuKeys = properties.keys();
-			
-			while (enuKeys.hasMoreElements()) {
 				
-				String key = (String) enuKeys.nextElement();
-				System.out.println(key + ": " );
-				if(key.equals("deviceType") ){
-					
-					deviceType = properties.getProperty(key);
-					
-				}
-				if(key.equals("deviceId")){
-					deviceId = properties.getProperty(key);
-					
-				}
-				if(key.equals("authKey")){
-					authKey = properties.getProperty(key);
-					
-				}
-				if(key.equals("authToken")){
-					authToken = properties.getProperty(key);
-				}
-				//System.out.println(key + ": " + value);
-			}
-			opt.put("authKey", authKey);
-			opt.put("authToken", authToken);
+			
+			
+			opt.put("authKey", properties.getProperty("authKey"));
+			opt.put("authToken", properties.getProperty("authToken"));
 
-			DeviceFactory factory = new DeviceFactory(opt);
-			//String metadata = "";
-			//boolean device = factory.deleteDevice(deviceType, deviceId);
+		DeviceFactory factory = new DeviceFactory(opt);
 		
-			assertEquals("Device didn't exist...", false, factory.deleteDevice(deviceType, deviceId));
-			assertEquals("Device deregistered....", true, factory.deleteDevice(deviceType, deviceId));
+		
+			assertEquals("Device didn't exist...", false, factory.deleteDevice(properties.getProperty("deviceType"), properties.getProperty("deviceId")));
+			assertEquals("Device deregistered....", true, factory.deleteDevice(properties.getProperty("deviceType"), properties.getProperty("deviceId")));
+			
+		
+		
 			
 			
 		} catch (FileNotFoundException e) {
@@ -70,6 +51,8 @@ public class TestDeleteDevices extends TestCase {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
+		}
+
+
 
 }
