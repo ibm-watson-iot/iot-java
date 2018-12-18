72c039c46e043311733626ae27286381589639bf
miketran <miketran@us.ibm.com>
2018-12-18 17:04:56 -0600

    Update DM tests

---
+++ src/test/java/com/ibm/iotf/client/device/DeviceManagementTest3.java
@@ -0,0 +1,173 @@
+package com.ibm.iotf.client.device;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
+import java.util.Random;
+
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.google.gson.JsonObject;
+import com.ibm.iotf.client.IoTFCReSTException;
+import com.ibm.iotf.client.api.APIClient;
+import com.ibm.iotf.devicemgmt.DeviceData;
+import com.ibm.iotf.devicemgmt.DeviceFirmware;
+import com.ibm.iotf.devicemgmt.DeviceFirmware.FirmwareState;
+import com.ibm.iotf.devicemgmt.DeviceInfo;
+import com.ibm.iotf.devicemgmt.DeviceMetadata;
+import com.ibm.iotf.devicemgmt.device.ManagedDevice;
+import com.ibm.iotf.test.common.TestEnv;
+import com.ibm.iotf.test.common.TestHelper;
+import com.ibm.iotf.util.LoggerUtility;;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DeviceManagementTest3 {
+	private static final String CLASS_NAME = DeviceManagementTest3.class.getName();
+	
+	private static Random random = new Random();
+	private static APIClient apiClient = null;
+	private static ManagedDevice dmClient = null;
+	private static final String DEVICE_TYPE = "DevMgmtType3";
+	private static final String DEVICE_ID = "DevMgmtDev3";
+	private static final String APP_ID = "DevMgmtApp3";
+	
+	/**
+	 * This method builds the device objects required to create the
+	 * ManagedClient
+	 * 
+	 * @param propertiesFile
+	 * @throws Exception
+	 */
+	private static void createManagedClient(Properties deviceProps) throws Exception {
+		DeviceFirmware firmware = new DeviceFirmware.Builder().
+				version("1.0.1").
+				name("iot-arm.deb").
+				url("").
+				verifier("12345").
+				state(FirmwareState.IDLE).				
+				build();
+		
+		DeviceInfo deviceInfo = new DeviceInfo.Builder().
+				serialNumber("1234errrrrr").
+				manufacturer("IBM").
+				model("T450").
+				deviceClass("A+").
+				description("Lenovo ThinkPad").
+				fwVersion("Windows10").
+				hwVersion("1.2.3").
+				descriptiveLocation("ELGC-6F-C138").
+				build();
+		
+		/**
+		 * Create a DeviceMetadata object
+		 */
+		JsonObject data = new JsonObject();
+		data.addProperty("customField", "customValue");
+		DeviceMetadata metadata = new DeviceMetadata(data);
+		metadata.setMetadata(new JsonObject());
+		
+		DeviceData deviceData = new DeviceData.Builder().
+						 deviceInfo(deviceInfo).
+						 deviceFirmware(firmware).
+						 metadata(metadata).
+						 build();
+		
+		dmClient = new ManagedDevice(deviceProps, deviceData);
+	}
+	
+	
+	@BeforeClass
+	public static void oneTimeSetUp() {
+		
+		final String METHOD = "oneTimeSetUp";
+		
+		Properties appProps = TestEnv.getAppProperties(APP_ID, false, null, null);
+		try {
+			apiClient = new APIClient(appProps);
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+			return;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		boolean exist = false;
+		
+		try {
+			exist = apiClient.isDeviceTypeExist(DEVICE_TYPE);
+		} catch (IoTFCReSTException e) {
+			e.printStackTrace();
+		}
+		
+		if (!exist) {
+			try {
+				TestHelper.addDeviceType(apiClient, DEVICE_TYPE);
+				LoggerUtility.info(CLASS_NAME, METHOD, "Device type " + DEVICE_TYPE + " has been created.");
+			} catch (IoTFCReSTException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		try {
+			TestHelper.deleteDevice(apiClient, DEVICE_TYPE, DEVICE_ID);
+		} catch (IoTFCReSTException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			TestHelper.registerDevice(apiClient, DEVICE_TYPE, DEVICE_ID, TestEnv.getDeviceToken());
+		} catch (IoTFCReSTException e) {
+			e.printStackTrace();
+		}
+		
+		Properties deviceProps = TestEnv.getDeviceProperties(DEVICE_TYPE, DEVICE_ID);
+
+		try {
+			createManagedClient(deviceProps);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	@AfterClass
+	public static void oneTimeCleanup() throws Exception {
+		final String METHOD = "oneTimeCleanup";
+		
+		if (apiClient != null) {
+			TestHelper.deleteDevice(apiClient, DEVICE_TYPE, DEVICE_ID);
+		}
+		LoggerUtility.info(CLASS_NAME, METHOD, "completed."); 
+	}	
+	
+	@Test
+	public void test01Errorcodes() {
+		final String METHOD = "test01Errorcodes";
+		boolean status = false;
+		try {
+			status = dmClient.sendManageRequest(0, true, false);
+			LoggerUtility.info(CLASS_NAME, METHOD, "send manage request, success = " + status); 
+		} catch (MqttException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+		
+		int errorCode = random.nextInt(500);
+		int rc = dmClient.addErrorCode(errorCode);
+		assertTrue("Errorcode addition unsuccessfull", rc==200);
+		
+		// Let us clear the errorcode now
+		rc = dmClient.clearErrorCodes();
+		assertTrue("clear Errorcode operation is unsuccessfull", rc==200);
+	}
+
+}
--- src/test/java/com/ibm/iotf/test/suites/TestSuite.java
+++ src/test/java/com/ibm/iotf/test/suites/TestSuite.java
@@ -9,12 +9,24 @@ import com.ibm.iotf.client.application.ApplicationEventSubscriptionTest;
 import com.ibm.iotf.client.application.api.BulkAPIOperationsTest;
 import com.ibm.iotf.client.device.DeviceCommandSubscriptionTest;
 import com.ibm.iotf.client.device.DeviceEventPublishTest;
-import com.ibm.iotf.client.device.DeviceManagementTest;
+import com.ibm.iotf.client.device.DeviceManagementTest1;
+import com.ibm.iotf.client.device.DeviceManagementTest2;
+import com.ibm.iotf.client.device.DeviceManagementTest3;
+import com.ibm.iotf.client.device.DeviceManagementTest4;
+import com.ibm.iotf.client.device.DeviceManagementTest5;
+import com.ibm.iotf.client.device.DeviceManagementTest6;
+import com.ibm.iotf.client.device.DeviceManagementTest7;
 import com.ibm.iotf.client.gateway.GatewayCommandSubscriptionTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
-	DeviceManagementTest.class,
+	DeviceManagementTest1.class,
+	DeviceManagementTest2.class,
+	DeviceManagementTest3.class,
+	DeviceManagementTest4.class,
+	DeviceManagementTest5.class,
+	DeviceManagementTest6.class,
+	DeviceManagementTest7.class,
 	DeviceEventPublishTest.class,
 	DeviceCommandSubscriptionTest.class,
 	GatewayCommandSubscriptionTest.class
