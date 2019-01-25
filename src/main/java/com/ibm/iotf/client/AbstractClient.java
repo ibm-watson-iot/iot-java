ef31a19a1444d44ccf4574a80b1fb94c96ac3cff
miketran <miketran@us.ibm.com>
2019-01-25 17:27:00 -0600

    Set Automatic Reconnect = true as default

--- src/main/java/com/ibm/iotf/client/AbstractClient.java
+++ src/main/java/com/ibm/iotf/client/AbstractClient.java
@@ -615,7 +615,7 @@ public abstract class AbstractClient {
 	}
 	
 	public boolean isAutomaticReconnect() {
-		boolean enabled = false;
+		boolean enabled = true;
 		String value = options.getProperty("Automatic-Reconnect");
 		if (value != null) {
 			enabled = Boolean.parseBoolean(trimedValue(value));
