9819081a2e59a18f0de6c7256f0720906d6b9566
David Parker <parkerda@uk.ibm.com>
2019-04-30 13:25:54 +0100

    Update dependencies / Misc cleanup

--- src/main/java/com/ibm/internal/iotf/devicemgmt/handler/RebootRequestHandler.java
+++ src/main/java/com/ibm/internal/iotf/devicemgmt/handler/RebootRequestHandler.java
@@ -1,13 +1,10 @@
 /**
  *****************************************************************************
- Copyright (c) 2015-17 IBM Corporation and other Contributors.
+ Copyright (c) 2015-19 IBM Corporation and other Contributors.
  All rights reserved. This program and the accompanying materials
  are made available under the terms of the Eclipse Public License v1.0
  which accompanies this distribution, and is available at
  http://www.eclipse.org/legal/epl-v10.html
- Contributors:
- Mike Tran - Initial Contribution
- Sathiskumar Palaniappan - Initial Contribution
  *****************************************************************************
  *
  */
--- src/main/java/com/ibm/iotf/devicemgmt/resource/StringResource.java
+++ src/main/java/com/ibm/iotf/devicemgmt/resource/StringResource.java
@@ -1,12 +1,10 @@
 /**
  *****************************************************************************
- Copyright (c) 2015 IBM Corporation and other Contributors.
+ Copyright (c) 2015-19 IBM Corporation and other Contributors.
  All rights reserved. This program and the accompanying materials
  are made available under the terms of the Eclipse Public License v1.0
  which accompanies this distribution, and is available at
  http://www.eclipse.org/legal/epl-v10.html
- Contributors:
- Sathiskumar Palaniappan - Initial Contribution
  *****************************************************************************
  *
  */
